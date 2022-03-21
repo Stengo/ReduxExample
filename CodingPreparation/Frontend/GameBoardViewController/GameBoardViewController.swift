24572300f6393168eaae1b38cbc698f1d6b74618
Stengo <bastian.andelefski@trivago.com>
2022-03-21 12:31:41 +0100

    Present success from the game board

--- CodingPreparation/Frontend/GameBoardViewController/GameBoardViewController.swift
+++ CodingPreparation/Frontend/GameBoardViewController/GameBoardViewController.swift
@@ -69,5 +69,9 @@ class GameBoardViewController: SubscriberViewController<GameBoardViewData> {
             return
         }
         store.dispatch(WordSelectionAction.select(selectedWord))
+
+        if selectedWord.lowercased() == "success" {
+            present(SuccessViewController(), animated: true, completion: nil)
+        }
     }
 }
