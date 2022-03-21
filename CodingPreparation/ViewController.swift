e1f00c9b5c93d1a1f1e46fc8a1c76880d72d7860
Stengo <bastian.andelefski@trivago.com>
2022-03-21 08:45:12 +0100

    Initial Commit

---
+++ CodingPreparation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodingPreparation
+//
+//  Created by Bastian Andelefski on 21.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
