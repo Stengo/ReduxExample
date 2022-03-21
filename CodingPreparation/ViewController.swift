9534add9d69d79f40cf30e898a979f28d2c0f9d5
Stengo <bastian.andelefski@trivago.com>
2022-03-21 08:56:54 +0100

    Remove unnecessary default code

--- CodingPreparation/ViewController.swift
+++ CodingPreparation/ViewController.swift
@@ -1,19 +1,4 @@
-//
-//  ViewController.swift
-//  CodingPreparation
-//
-//  Created by Bastian Andelefski on 21.03.22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
-
