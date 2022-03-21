4981986f928fabf268fe5a7415ea5c3bb765c895
Stengo <bastian.andelefski@trivago.com>
2022-03-21 09:49:22 +0100

    Add a text view to the view controller

--- CodingPreparation/Frontend/ViewController.swift
+++ CodingPreparation/Frontend/ViewController.swift
@@ -1,4 +1,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    lazy var textView: UITextView = {
+        let textView = UITextView()
+        textView.textAlignment = .center
+        textView.font = .systemFont(ofSize: 24)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        view.addSubview(textView)
+        configureConstraints()
+
+        textView.text = "some text"
+    }
+
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            textView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            textView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            textView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
 }
